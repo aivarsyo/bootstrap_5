a47069dd97ccb232168a660f35353173d157ede6
aivarsyo <aiva0071@stud.kea.dk>
2020-08-26 10:37:04 +0200

    better animation

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,18 @@ import '../../node_modules/bootstrap';
 // CSS
 import "../scss/style.scss";
 
+import gsap from "gsap";
+import barba from '@barba/core';
+
 // --- initialize ---
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init(){
+
     menu();
+    initTransition();
+    
 }
 
 // --- *********** ---
@@ -31,3 +37,77 @@ span.addEventListener("click", function(){
 })
 }
 
+function pageTransition(){
+
+  const tl = gsap.timeline();
+
+  tl.to("ul.transition li", {
+    duration: .3,
+    scaleY: 1,
+    transformOrigin: "bottom left",
+    stagger: .1
+  })
+
+  tl.to("ul.transition li", {
+    duration: .3,
+    scaleY: 0,
+    transformOrigin: "bottom left",
+    stagger: .1,
+    delay: .1
+  })
+
+}
+
+function contentAnimation(){
+
+  const tl = gsap.timeline();
+
+  /* tl.from(".form-login", {
+    duration: 1.5,
+    translateY: 50,
+    opacity: 0
+  }) */
+
+  /* tl.to(".form-login", {
+    duration: 1.5,
+    translateY: 50,
+    opacity: 0
+  }) */
+}
+
+  function delayy(n){
+    n = n || 2000;
+    return new Promise(done => {
+      setTimeout(() => {
+        done();
+      }, n);
+    })
+  }
+
+function initTransition(){
+
+  barba.init({
+    sync: true,
+
+    transitions: [{
+
+      async leave(data){
+
+         const done = this.async();
+
+         pageTransition();
+         await delayy(1000);
+         done();
+      },
+
+    async enter(data) {
+      contentAnimation();
+    },
+    
+    async once(data) {
+      contentAnimation();
+    }  
+
+    }]
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -104,7 +104,7 @@ module.exports = {
 
         new HtmlWebPackPlugin({
             template: "./src/flex.html",
-            filename: "./grid.html"
+            filename: "./flex.html"
         }),
 
 
